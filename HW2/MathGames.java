79a345feb7d49c1ef4a0e54855e2ebff4e3fe4e0
Arek Ohanian <Ohaniana@go.stockton.edu>
2023-03-23 15:11:31 -0400

    changed some words

--- HW2/MathGames.java
+++ HW2/MathGames.java
@@ -121,8 +121,7 @@ public class MathGames {
         double tip = input.nextDouble();
         int tipammount = ((int) Math.ceil(tip(bill, people, tip)));
         System.out.println("Each person will tip " + tipammount + "$");
-        System.out.println("Thank you for playing/not crashing my program why did i put so much work into this time for the 2 hours of unskippable credits");
-        System.out.println("Made by: Github co-pilot");
+        System.out.println("Thank you for playing/not crashing my program");
         input.close();
     }
   
